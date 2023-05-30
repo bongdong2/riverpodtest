a360d096bfb8869c12d2618a6754ee592aebbe44
bongdong2 <seungui821@ohmycompany.com>
2023-05-30 15:33:50 +0900

    매무리

--- lib/screen/code_generation_screen.dart
+++ lib/screen/code_generation_screen.dart
@@ -8,6 +8,8 @@ class CodeGenerationScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    print('build');
+
     final state1 = ref.watch(gStateProvider);
     final state2 = ref.watch(gStateFutureProvider);
     final state3 = ref.watch(gStateFuture2Provider);
@@ -15,7 +17,6 @@ class CodeGenerationScreen extends ConsumerWidget {
       number1: 10,
       number2: 20,
     ));
-    final state5 = ref.watch(gStateNotifierProvider);
 
     return DefaultLayout(
       title: 'CodeGenerationScreen',
@@ -50,7 +51,21 @@ class CodeGenerationScreen extends ConsumerWidget {
             loading: () => Center(child: CircularProgressIndicator()),
           ),
           Text('State 4 : $state4'),
-          Text('State 5 : $state5'),
+          Consumer(
+            builder: (context, ref, child) {
+              print('builder build');
+              final state5 = ref.watch(gStateNotifierProvider);
+              return Row(
+                children: [
+                  Text('State 5 : $state5'),
+                  if(child != null)
+                    child
+                ],
+              );
+            },
+            child: Text('hello'), // 여기에 있는 위젯이 위의 child가 되어 렌더링 된다.
+            // 퍼포먼스상 한 번만 빌드 되어야 하는 위젯을 여기에 넣는다.
+          ),
           Row(
             children: [
               ElevatedButton(
@@ -66,7 +81,14 @@ class CodeGenerationScreen extends ConsumerWidget {
                 child: Text('Decrement'),
               ),
             ],
-          )
+          ),
+          ElevatedButton(
+            onPressed: () {
+              // 2.0부터 생김
+              ref.invalidate(gStateNotifierProvider);
+            },
+            child: Text('Invalidate'),
+          ),
         ],
       ),
     );
